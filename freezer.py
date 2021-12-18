6403c7e9e9fcba5b4a2d891627e4ad401f65918c
ab12gu <ab18gu@gmail.com>
2021-12-18 10:49:06 -0800

    Add poetry project template [dependencies not linked yet]

--- freezer.py
+++ freezer.py
@@ -1,5 +1,14 @@
-from flask_frozen import Freezer
-from main import app
+# filename: freezer.py
+#
+# by: Abhay Gupta
+# date created: 11-12-21
+#
+# desc: runs flask freezer to freeze website into a static site for github pages
+
+# NOTE: By default, it will silently overwrite files in the build directory, and remove those it did not create.
+
+from flask_frozen import Freezer # import freezer
+from main import app # import app
 
 freezer = Freezer(app)
 
