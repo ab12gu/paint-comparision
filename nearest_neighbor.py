fe339b7d2eb21f472cea68beff129f61ed7c8a78
Abhay Gupta <ab18gu@gmail.com>
2022-01-22 17:59:16 -0800

    Add dependencies to poetry for local dependency management

--- nearest_neighbor.py
+++ nearest_neighbor.py
@@ -15,7 +15,7 @@ def nearest_neighbor():
     from sys import platform as sys_pf
     if sys_pf == 'darwin':
         import matplotlib
-        matplotlib.use("TkAgg")
+        # matplotlib.use("TkAgg")
 
     import json
     import pprint
